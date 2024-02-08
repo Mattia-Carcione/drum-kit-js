15b93718e43bf6534aa9883ebe2f76e2dab7e95c
Mattia <mattia@MacBook-Pro-di-Mattia.local>
2024-02-08 17:15:07 +0100

    add script

---
+++ script.js
@@ -0,0 +1,55 @@
+var drumButtons = document.querySelectorAll(".drum");
+
+for (let i = 0; i < drumButtons.length; i++) {
+    drumButtons[i].addEventListener('click', function () {
+        var buttonInnerHTML = drumButtons[i].innerHTML;
+        playSound(buttonInnerHTML);
+    });
+}
+
+document.addEventListener('keydown', function (event) {
+    playSound(event.key);
+})
+
+function playSound(event) {
+    switch (event) {
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "a":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        case "s":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "d":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "j":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "k":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case "l":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        default:
+            console.error("Wrong key pressed");
+            break;
+    }
+}
